6bdcce87f0c41d7f94ab2609fe12cfca12971c24
RuizClapesLucca <lruizc2402@alumno.ipn.mx>
2026-05-18 19:45:38 -0600

    Correciones

--- 07Crud/server/server.js
+++ 07Crud/server/server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000; 
 const db = mysql.createPool({host:'localhost',user:'root',password:'Elpro109@',database:'pnt_practica1',waitForConnections:true,connectionLimit:10,queueLimit:0}).promise(); // crear pool de conexiones MySQL y usar promesas
 app.use(express.json()); 
-app.use(express.static(path.join(__dirname,'public'))); 
+app.use(express.static('../public'));
 app.get('/api/test',(req,res)=>res.json({ok:true})); 
 app.post('/api/test',async(req,res)=>{const data=req.body;res.json({received:data})}); 
 app.listen(PORT,()=>console.log(`Servidor inicializado en el puerto ${PORT}`)); 
